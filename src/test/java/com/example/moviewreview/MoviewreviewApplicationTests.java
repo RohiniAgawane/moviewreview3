0d66063ce0f48b9ed6f22f7f111f63b5deca481f
ADMIN <rohini.shinde@gmail.com>
2019-11-29 12:35:27 +0530

    first commit

---
+++ src/test/java/com/example/moviewreview/MoviewreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviewreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviewreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
